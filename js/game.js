fa768e78288a9e1cb3900e4fd2104148653da655
Edu Benito <eduardbenito@icloud.com>
2018-07-09 15:10:47 +0200

    refactor to control screens in game instead in main

--- js/game.js
+++ js/game.js
@@ -1,17 +1,71 @@
-function Game(render, endGame) {
+function Game(render) {
   this.render = render;
-  this.endGame = endGame;
-  
+
+  this._INIT_STATE = 0;
+  this._GAME_STATE = 1
+  this._END_STATE = 2;
+
   this._intervalID = undefined;
+  this._nowPlaying = false;
+  this._nowGameOver = false;
 }
 
 Game.prototype.init = function() {
+  this._assignControlsToKeys();
+  this._drawSplash();
+}
+
+Game.prototype._beginGame = function() {
   this.render.resetCanvas();
-  this._intervalID = setInterval(this._gameOver.bind(this), 2000);
+  this._changeState(this._GAME_STATE);
 }
 
 Game.prototype._gameOver = function() {
-  clearInterval(this._intervalID);
   this._intervalID = undefined;
-  endGame();
+  this.render.drawGameOver();
+  this._changeState(this._END_STATE);
+}
+
+Game.prototype._changeState = function(newState) {
+  switch (newState) {
+    case this._INIT_STATE:
+      this._nowPlaying = false;
+      this._nowGameOver = false;
+      break;
+
+    case this._GAME_STATE:
+      this._nowPlaying = true;
+      this._nowGameOver = false;
+      break;
+
+    case this._END_STATE:
+      this._nowPlaying = false;
+      this._nowGameOver = true;
+      break;
+  }
+} 
+
+Game.prototype._drawSplash = function() {
+  this.render.drawSplash();
+  this._changeState(this._INIT_STATE);
+}
+
+Game.prototype._assignControlsToKeys = function () {
+  document.onkeydown = function (e) {
+    switch (e.keyCode) {
+      case 27: //ESC
+        if (this._nowPlaying) {
+          this._gameOver();
+        }
+        break;
+
+      case 32: //SPACE
+        if (!this._nowPlaying && !this._nowGameOver) {
+          this._beginGame();
+        } else if (this._nowGameOver) {
+          this._drawSplash();
+        }
+        break;
+    }
+  }.bind(this);
 }
--- js/main.js
+++ js/main.js
@@ -1,36 +1,7 @@
-var restart = false;
-
-function endGame() {
+function init() {
   var render = new Render(document.querySelector('canvas').getContext('2d'));
-  render.drawGameOver();
-  restart = true;
-}
-
-function buildGame () {
-  var render = new Render(document.querySelector('canvas').getContext('2d'));
-  var game = new Game(render, endGame);
+  var game = new Game(render);
   game.init();
 }
 
-function buildSplash() {
-  var render = new Render(document.querySelector('canvas').getContext('2d'));
-  render.drawSplash();
-  restart = false;
-}
-
-function keyDown(e) {
-  if (e.keyCode === 32) {
-    if (restart) {
-      buildSplash();
-    } else {
-      buildGame();
-    }
-  }
-}
-
-function init() {
-  buildSplash();
-}
-
-document.addEventListener("keydown", keyDown);
 document.fonts.load('10pt "hyperspace"').then(init);
